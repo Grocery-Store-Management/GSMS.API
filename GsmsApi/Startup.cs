7c8c25bba5344c2253163e5a533d09a3468e6562
Tran Phong <n.tranphongse@gmail.com>
2022-02-09 12:43:11 +0700

    Active Swagger to Production

--- GsmsApi/Startup.cs
+++ GsmsApi/Startup.cs
@@ -40,9 +40,9 @@ namespace GsmsApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GsmsApi v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GsmsApi v1"));
 
             app.UseHttpsRedirection();
 
