7e38819b06481e91151a55a3f3f2a3c18b2b0cea
Tran Phong <n.tranphongse@gmail.com>
2022-02-10 21:07:50 +0700

    Routes for Category

--- DataAccessLibrary/BusinessEntity/CategoryBusinessEntity.cs
+++ DataAccessLibrary/BusinessEntity/CategoryBusinessEntity.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLibrary.Interfaces;
+﻿using BusinessObjectLibrary;
+using DataAccessLibrary.Interfaces;
+using GsmsLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,50 @@ namespace DataAccessLibrary.BusinessEntity
         public CategoryBusinessEntity(IUnitOfWork work)
         {
             this.work = work;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await work.Categories.GetAllAsync();
+        }
+
+        public async Task<Category> GetCategoryAsync(string id)
+        {
+            return await work.Categories.GetAsync(id);
+        }
+
+        public async Task<Category> AddCategoryAsync(Category newCategory)
+        {
+            newCategory.Id = GsmsUtils.CreateGuiId();
+            newCategory.IsDeleted = false;
+            await work.Categories.AddAsync(newCategory);
+            work.Save();
+            return newCategory;
+        }
+
+        public async Task<Category> UpdateCategoryAsync(Category updatedCategory)
+        {
+            Category category = await work.Categories.GetAsync(updatedCategory.Id);
+            if (category == null)
+            {
+                throw new Exception("Category is not existed!!");
+            }
+            category.Name = updatedCategory.Name;
+            category.IsDeleted = updatedCategory.IsDeleted;
+            work.Categories.Update(category);
+            work.Save();
+            return category;
+        }
+
+        public async Task DeleteCategoryAsync(string id)
+        {
+            Category category = await work.Categories.GetAsync(id);
+            if (category == null)
+            {
+                throw new Exception("Category is not existed!!");
+            }
+            work.Categories.Delete(category);
+            work.Save();
         }
     }
 }
---
+++ GsmsApi/Controllers/CategoryController.cs
@@ -0,0 +1,138 @@
+﻿using BusinessObjectLibrary;
+using DataAccessLibrary.BusinessEntity;
+using DataAccessLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GsmsApi.Controllers
+{
+    //PhongNT
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private CategoryBusinessEntity categoryEntity;
+        public CategoryController(IUnitOfWork work)
+        {
+            categoryEntity = new CategoryBusinessEntity(work);
+        }
+
+        // GET: api/<CategoryController>
+        /// <summary>
+        /// Get All the Categories
+        /// </summary>
+        /// <returns>List of categories</returns>
+        [HttpGet]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(IEnumerable<Category>), 200)]
+        public async Task<IActionResult> GetAsync()
+        {
+            try
+            {
+                IEnumerable<Category> categories = await categoryEntity.GetCategoriesAsync();
+                return StatusCode(200, categories);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get category based on ID
+        /// </summary>
+        /// <param name="id">The ID string of the category</param>
+        /// <returns>The existed category</returns>
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(typeof(Category), 200)]
+        public async Task<IActionResult> GetAsync(string id)
+        {
+            try
+            {
+                Category category = await categoryEntity.GetCategoryAsync(id);
+                if (category == null)
+                {
+                    return StatusCode(204);
+                }
+                return StatusCode(200, category);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // POST api/<CategoryController>
+        /// <summary>
+        /// Add a new Category
+        /// </summary>
+        /// <param name="newCategory">New category to be added, NAME property of the category must be provided</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(Category), 201)]
+        public async Task<IActionResult> Post([FromBody] Category newCategory)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(newCategory.Name))
+                {
+                    throw new Exception("Category name is empty!!");
+                }
+                Category addedCategory = await categoryEntity.AddCategoryAsync(newCategory);
+                return StatusCode(201, addedCategory);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // PUT api/<CategoryController>/5
+        /// <summary>
+        /// Update Category
+        /// </summary>
+        /// <param name="id">ID of the category to be updated</param>
+        /// <param name="updatedCategory">New information to update</param>
+        /// <returns>The updated category</returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(Category), 201)]
+        public async Task<IActionResult> Put(string id, [FromBody] Category updatedCategory)
+        {
+            try
+            {
+                if (!id.Equals(updatedCategory.Id))
+                {
+                    throw new Exception("The ID is not the same!!");
+                }
+                Category category = await categoryEntity.UpdateCategoryAsync(updatedCategory);
+                return StatusCode(201, category);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(204)]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                await categoryEntity.DeleteCategoryAsync(id);
+                return StatusCode(204);
+            } catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
